9ed62042a45cbe06da36c61db9b4eb3c625a5e89
VioleHours <Holasoyviole@gmail.com>
2024-04-24 20:31:56 -0300

    improvement

---
+++ notas/src/components/Button.svelte
@@ -0,0 +1,16 @@
+<script>
+  let count = 0;
+  $: total = count;
+
+  function handleClick() {
+    count++;
+  }
+</script>
+
+<div class="Button">
+  <button on:click={handleClick}>Click me</button>
+  <div>You click {total} times</div>
+</div>
+
+<style>
+</style>
